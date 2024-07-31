abe37274634b54b17e3b895ce53118a1fdac28ef
Campaña <161412285+amesitos@users.noreply.github.com>
2024-07-31 12:06:20 +0200

    Delete ApiRESTBocaUrna2 directory

--- ApiRESTBocaUrna2/src/main/java/ec/com/webmarket/restful/persistence/CandidatoRepository.java
+++
@@ -1,7 +0,0 @@
-package ec.com.webmarket.restful.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ec.com.webmarket.restful.domain.Candidato;
-
-public interface CandidatoRepository extends JpaRepository<Candidato, Long> {}
--- ApiRESTBocaUrna2/src/main/java/ec/com/webmarket/restful/persistence/EstudianteRepository.java
+++
@@ -1,8 +0,0 @@
-package ec.com.webmarket.restful.persistence;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ec.com.webmarket.restful.domain.EstudianteNormal;
-
-public interface EstudianteRepository extends JpaRepository<EstudianteNormal, Long> {
-	
-}
