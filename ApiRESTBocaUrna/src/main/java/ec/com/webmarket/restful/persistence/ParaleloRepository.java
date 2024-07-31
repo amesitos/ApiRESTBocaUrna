8dc0cee48e58f3dad7562537960aacdf41a95167
Campaña <161412285+amesitos@users.noreply.github.com>
2024-07-31 23:05:39 +0200

    Delete ApiRESTBocaUrna directory

--- ApiRESTBocaUrna/src/main/java/ec/com/webmarket/restful/persistence/ParaleloRepository.java
+++
@@ -1,9 +0,0 @@
-package ec.com.webmarket.restful.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ec.com.webmarket.restful.domain.Paralelo;
-
-public interface ParaleloRepository extends JpaRepository<Paralelo, Long> {
-	
-}
